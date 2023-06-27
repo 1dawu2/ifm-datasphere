1573f322ec9039016614c8843c633e8b0e809e5b
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-06-27 18:51:29 +0200

    u

--- src/components/DataSphere.js
+++ src/components/DataSphere.js
@@ -1,11 +1,10 @@
-(function () {
-  let _shadowRoot;
-  let _oAuthURL;
-  let _clientID;
-  let _apiSecret;
+let _shadowRoot;
+let _oAuthURL;
+let _clientID;
+let _apiSecret;
 
-  let tmpl = document.createElement("template");
-  tmpl.innerHTML = `
+let tmpl = document.createElement("template");
+tmpl.innerHTML = `
     <style>
     </style>
     <div id="ifm_datasphere" name="ifm_datasphere">
@@ -41,191 +40,188 @@
     </script>
   `;
 
-  class IFMDataSphere extends HTMLElement {
+class IFMDataSphere extends HTMLElement {
 
-    constructor() {
-      super();
+  constructor() {
+    super();
 
-      // const dwc = require("@sap/dwc-cli");
-      // const MY_HOST = "https://dwc-infomotion.eu10.hcs.cloud.sap/";
-      // console.log(commands);
+    // const dwc = require("@sap/dwc-cli");
+    // const MY_HOST = "https://dwc-infomotion.eu10.hcs.cloud.sap/";
+    // console.log(commands);
 
-      _shadowRoot = this.attachShadow({
-        mode: "open"
-      });
+    _shadowRoot = this.attachShadow({
+      mode: "open"
+    });
 
-      _shadowRoot.appendChild(tmpl.content.cloneNode(true));
+    _shadowRoot.appendChild(tmpl.content.cloneNode(true));
 
-      this._export_settings = {};
-      this._export_settings.restapiurl = "";
+    this._export_settings = {};
+    this._export_settings.restapiurl = "";
 
-      // this.executeTaskChain();
+    // this.executeTaskChain();
 
+  }
+
+  onCustomWidgetResize(width, height) {
+  }
+
+  connectedCallback() {
+  }
+
+  disconnectedCallback() {
+  }
+
+  onCustomWidgetBeforeUpdate(changedProperties) {
+    if ("designMode" in changedProperties) {
+      this._designMode = changedProperties["designMode"];
     }
+  }
 
-    onCustomWidgetResize(width, height) {
-    }
+  onCustomWidgetAfterUpdate(changedProperties) {
+    // this.buildUI(changedProperties, this);
+  }
 
-    connectedCallback() {
-    }
+  // SETTINGS
+  get restapiurl() {
+    return this._export_settings.restapiurl;
+  }
+  set restapiurl(value) {
+    this._export_settings.restapiurl = value;
+  }
 
-    disconnectedCallback() {
-    }
+  static get observedAttributes() {
+    return [
+      "restapiurl",
+    ];
+  }
 
-    onCustomWidgetBeforeUpdate(changedProperties) {
-      if ("designMode" in changedProperties) {
-        this._designMode = changedProperties["designMode"];
+  executeTaskChain() {
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        console.log(this.responseText);
       }
+    });
+
+    xhr.open("POST", "https://dwc-infomotion.eu10.hcs.cloud.sap/dwaas-core/tf/BU_SINGER/taskchains/Task_Chain_1/start");
+    xhr.setRequestHeader("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vZHdjLWluZm9tb3Rpb24uYXV0aGVudGljYXRpb24uZXUxMC5oYW5hLm9uZGVtYW5kLmNvbS90b2tlbl9rZXlzIiwia2lkIjoiZGVmYXVsdC1qd3Qta2V5LTI4Njg5MjQxMyIsInR5cCI6IkpXVCIsImppZCI6ICJJRysyZjFVbWhtbytXNEdwRDRYWUFPME5JUnlBWEd3emdFQUZSdklaS3Y4PSJ9.eyJqdGkiOiJmYzMzNGIyMWJiZTA0MzVlODRlMTk3NDU3NTNlZmI1YSIsImV4dF9hdHRyIjp7ImVuaGFuY2VyIjoiWFNVQUEiLCJzdWJhY2NvdW50aWQiOiIwN2EzMThlNS04ODdmLTRiZWUtOWY1MC0xOTg3ODg3MTE2MzgiLCJ6ZG4iOiJkd2MtaW5mb21vdGlvbiIsInNlcnZpY2VpbnN0YW5jZWlkIjoiNTc0NzcwNTItNDI0Yi00ZmZmLWFmYjctY2MwNTA4OTUzNDVjIn0sInhzLnN5c3RlbS5hdHRyaWJ1dGVzIjp7InhzLnNhbWwuZ3JvdXBzIjpbInNhYyJdLCJ4cy5yb2xlY29sbGVjdGlvbnMiOlsic2FjLnVzZXJzIl19LCJnaXZlbl9uYW1lIjoiZGF2aWQud3VybSIsInhzLnVzZXIuYXR0cmlidXRlcyI6e30sImZhbWlseV9uYW1lIjoiaW5mb21vdGlvbi5kZSIsInN1YiI6IjJlMzU0ZDU2LThhZmQtNDEwYS05Y2RlLWU2ODAyYTk5Yzc0MyIsInNjb3BlIjpbIm9wZW5pZCIsImFwcHJvdXRlci1zYWMtc2FjZXUxMCF0MzY1MC5zYXAuZnBhLnVzZXIiLCJ1YWEudXNlciJdLCJjbGllbnRfaWQiOiJzYi1hNmQwOTk2OC05Y2YyLTQ5NDAtYTcyNS1iYzY5ZjNlODc1ZmYhYjEwNjM0M3xjbGllbnQhYjM2NTAiLCJjaWQiOiJzYi1hNmQwOTk2OC05Y2YyLTQ5NDAtYTcyNS1iYzY5ZjNlODc1ZmYhYjEwNjM0M3xjbGllbnQhYjM2NTAiLCJhenAiOiJzYi1hNmQwOTk2OC05Y2YyLTQ5NDAtYTcyNS1iYzY5ZjNlODc1ZmYhYjEwNjM0M3xjbGllbnQhYjM2NTAiLCJncmFudF90eXBlIjoiYXV0aG9yaXphdGlvbl9jb2RlIiwidXNlcl9pZCI6IjJlMzU0ZDU2LThhZmQtNDEwYS05Y2RlLWU2ODAyYTk5Yzc0MyIsIm9yaWdpbiI6ImhhbmFjbG91ZHNlcnZpY2VzLWV1LmFjY291bnRzLm9uZGVtYSIsInVzZXJfbmFtZSI6ImRhdmlkLnd1cm1AaW5mb21vdGlvbi5kZSIsImVtYWlsIjoiZGF2aWQud3VybUBpbmZvbW90aW9uLmRlIiwiYXV0aF90aW1lIjoxNjg3ODc5NDY3LCJyZXZfc2lnIjoiMTYxMzM2ODYiLCJpYXQiOjE2ODc4Nzk2ODYsImV4cCI6MTY4Nzg4MzI4NiwiaXNzIjoiaHR0cHM6Ly9kd2MtaW5mb21vdGlvbi5hdXRoZW50aWNhdGlvbi5ldTEwLmhhbmEub25kZW1hbmQuY29tL29hdXRoL3Rva2VuIiwiemlkIjoiMDdhMzE4ZTUtODg3Zi00YmVlLTlmNTAtMTk4Nzg4NzExNjM4IiwiYXVkIjpbImFwcHJvdXRlci1zYWMtc2FjZXUxMCF0MzY1MC5zYXAuZnBhIiwic2ItYTZkMDk5NjgtOWNmMi00OTQwLWE3MjUtYmM2OWYzZTg3NWZmIWIxMDYzNDN8Y2xpZW50IWIzNjUwIiwidWFhIiwib3BlbmlkIl19.GdYP2i50VR3-KfL1tY7KZ7AlOhNB7AF_WIj8afT2JWXSrBqoQgheAHF9Wv9aw8uXjanTFO-t9W7XE3V1yC_b2Jzo0Ng8TCa-3y-7nOxQfry10lr_qAqk_nRjD9cuWlEeseYe5VhpGL1a4M3Q5bO50-hgWEEXtyjlUpXbhD8V_p1NIypn3eEGlsNZtxOGOlNWBDv9_O0_6yVRLNyk5gpLFfqq1Ddu5sp_o4nToz4VXcmpn6MvrOU449v3R9m_MMiIsq3pPjfhYb2QD2mvMcHi7jCbLXNidlHcPDC1XgH6yL6IHrNtK33seMG7XWrqK7Yw3uypxCW_mdjINCyu9uWgCg");
+    xhr.setRequestHeader("Cookie", "JSESSIONID=s%3A_EvTAY82EYX4VjpkDv0enuqPGJ9ucLLg.ni2gKCxZCFc2GQ%2B9dOIB3EJCF%2B5q0G15gj0w8KvuT%2B4; __VCAP_ID__=eb6a6391-93e0-4a14-5225-796d");
+
+    xhr.send();
+
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue != newValue) {
+      this[name] = newValue;
     }
+  }
 
-    onCustomWidgetAfterUpdate(changedProperties) {
-      // this.buildUI(changedProperties, this);
-    }
+  buildUI(changedProperties, that) {
+    var that_ = that;
 
-    // SETTINGS
-    get restapiurl() {
-      return this._export_settings.restapiurl;
-    }
-    set restapiurl(value) {
-      this._export_settings.restapiurl = value;
-    }
+    let content = document.createElement('div');
+    content.slot = "content";
+    that_.appendChild(content);
 
-    static get observedAttributes() {
-      return [
-        "restapiurl",
-      ];
-    }
+    sap.ui.define(
+      [
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/core/library",
+      ],
+      function (Controller) {
+        "use strict";
 
-    executeTaskChain() {
+        return Controller.extend("ifm.hack.initial", {
 
-      var xhr = new XMLHttpRequest();
-      xhr.withCredentials = true;
+          onInit: function (oEvent) {
 
-      xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === 4) {
-          console.log(this.responseText);
-        }
-      });
+          },
 
-      xhr.open("POST", "https://dwc-infomotion.eu10.hcs.cloud.sap/dwaas-core/tf/BU_SINGER/taskchains/Task_Chain_1/start");
-      xhr.setRequestHeader("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vZHdjLWluZm9tb3Rpb24uYXV0aGVudGljYXRpb24uZXUxMC5oYW5hLm9uZGVtYW5kLmNvbS90b2tlbl9rZXlzIiwia2lkIjoiZGVmYXVsdC1qd3Qta2V5LTI4Njg5MjQxMyIsInR5cCI6IkpXVCIsImppZCI6ICJJRysyZjFVbWhtbytXNEdwRDRYWUFPME5JUnlBWEd3emdFQUZSdklaS3Y4PSJ9.eyJqdGkiOiJmYzMzNGIyMWJiZTA0MzVlODRlMTk3NDU3NTNlZmI1YSIsImV4dF9hdHRyIjp7ImVuaGFuY2VyIjoiWFNVQUEiLCJzdWJhY2NvdW50aWQiOiIwN2EzMThlNS04ODdmLTRiZWUtOWY1MC0xOTg3ODg3MTE2MzgiLCJ6ZG4iOiJkd2MtaW5mb21vdGlvbiIsInNlcnZpY2VpbnN0YW5jZWlkIjoiNTc0NzcwNTItNDI0Yi00ZmZmLWFmYjctY2MwNTA4OTUzNDVjIn0sInhzLnN5c3RlbS5hdHRyaWJ1dGVzIjp7InhzLnNhbWwuZ3JvdXBzIjpbInNhYyJdLCJ4cy5yb2xlY29sbGVjdGlvbnMiOlsic2FjLnVzZXJzIl19LCJnaXZlbl9uYW1lIjoiZGF2aWQud3VybSIsInhzLnVzZXIuYXR0cmlidXRlcyI6e30sImZhbWlseV9uYW1lIjoiaW5mb21vdGlvbi5kZSIsInN1YiI6IjJlMzU0ZDU2LThhZmQtNDEwYS05Y2RlLWU2ODAyYTk5Yzc0MyIsInNjb3BlIjpbIm9wZW5pZCIsImFwcHJvdXRlci1zYWMtc2FjZXUxMCF0MzY1MC5zYXAuZnBhLnVzZXIiLCJ1YWEudXNlciJdLCJjbGllbnRfaWQiOiJzYi1hNmQwOTk2OC05Y2YyLTQ5NDAtYTcyNS1iYzY5ZjNlODc1ZmYhYjEwNjM0M3xjbGllbnQhYjM2NTAiLCJjaWQiOiJzYi1hNmQwOTk2OC05Y2YyLTQ5NDAtYTcyNS1iYzY5ZjNlODc1ZmYhYjEwNjM0M3xjbGllbnQhYjM2NTAiLCJhenAiOiJzYi1hNmQwOTk2OC05Y2YyLTQ5NDAtYTcyNS1iYzY5ZjNlODc1ZmYhYjEwNjM0M3xjbGllbnQhYjM2NTAiLCJncmFudF90eXBlIjoiYXV0aG9yaXphdGlvbl9jb2RlIiwidXNlcl9pZCI6IjJlMzU0ZDU2LThhZmQtNDEwYS05Y2RlLWU2ODAyYTk5Yzc0MyIsIm9yaWdpbiI6ImhhbmFjbG91ZHNlcnZpY2VzLWV1LmFjY291bnRzLm9uZGVtYSIsInVzZXJfbmFtZSI6ImRhdmlkLnd1cm1AaW5mb21vdGlvbi5kZSIsImVtYWlsIjoiZGF2aWQud3VybUBpbmZvbW90aW9uLmRlIiwiYXV0aF90aW1lIjoxNjg3ODc5NDY3LCJyZXZfc2lnIjoiMTYxMzM2ODYiLCJpYXQiOjE2ODc4Nzk2ODYsImV4cCI6MTY4Nzg4MzI4NiwiaXNzIjoiaHR0cHM6Ly9kd2MtaW5mb21vdGlvbi5hdXRoZW50aWNhdGlvbi5ldTEwLmhhbmEub25kZW1hbmQuY29tL29hdXRoL3Rva2VuIiwiemlkIjoiMDdhMzE4ZTUtODg3Zi00YmVlLTlmNTAtMTk4Nzg4NzExNjM4IiwiYXVkIjpbImFwcHJvdXRlci1zYWMtc2FjZXUxMCF0MzY1MC5zYXAuZnBhIiwic2ItYTZkMDk5NjgtOWNmMi00OTQwLWE3MjUtYmM2OWYzZTg3NWZmIWIxMDYzNDN8Y2xpZW50IWIzNjUwIiwidWFhIiwib3BlbmlkIl19.GdYP2i50VR3-KfL1tY7KZ7AlOhNB7AF_WIj8afT2JWXSrBqoQgheAHF9Wv9aw8uXjanTFO-t9W7XE3V1yC_b2Jzo0Ng8TCa-3y-7nOxQfry10lr_qAqk_nRjD9cuWlEeseYe5VhpGL1a4M3Q5bO50-hgWEEXtyjlUpXbhD8V_p1NIypn3eEGlsNZtxOGOlNWBDv9_O0_6yVRLNyk5gpLFfqq1Ddu5sp_o4nToz4VXcmpn6MvrOU449v3R9m_MMiIsq3pPjfhYb2QD2mvMcHi7jCbLXNidlHcPDC1XgH6yL6IHrNtK33seMG7XWrqK7Yw3uypxCW_mdjINCyu9uWgCg");
-      xhr.setRequestHeader("Cookie", "JSESSIONID=s%3A_EvTAY82EYX4VjpkDv0enuqPGJ9ucLLg.ni2gKCxZCFc2GQ%2B9dOIB3EJCF%2B5q0G15gj0w8KvuT%2B4; __VCAP_ID__=eb6a6391-93e0-4a14-5225-796d");
+          onButtonPress: function (oEvent) {
+            var this_ = this;
 
-      xhr.send();
+            var partnernumber = oView.byId("input").getValue();
+            console.log(partnernumber);
+            this_.wasteTime();
 
-    }
+            var CLIENT_ID_str = 'REPLACE_WITH_CLIENT_ID';
+            var CLIENT_SECRET_str = 'REPLACE_WITH_CLIENT_SECRET';
 
-    attributeChangedCallback(name, oldValue, newValue) {
-      if (oldValue != newValue) {
-        this[name] = newValue;
-      }
-    }
+            $.ajax({
+              type: 'POST',
+              url: "https://REPLACE_WITH_TOKEN_URL/uaa-security/oauth/token",
+              contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+              crossDomain: true,
+              cache: true,
+              dataType: 'json',
+              data: {
+                client_id: CLIENT_ID_str,
+                client_secret: CLIENT_SECRET_str,
+                grant_type: 'client_credentials',
+              },
 
-    buildUI(changedProperties, that) {
-      var that_ = that;
+              success: function (data) {
+                console.log(data);
 
-      let content = document.createElement('div');
-      content.slot = "content";
-      that_.appendChild(content);
+                var access_token = data.access_token;
 
-      sap.ui.define(
-        [
-          "sap/ui/core/mvc/Controller",
-          "sap/ui/core/library",
-        ],
-        function (Controller) {
-          "use strict";
+                $.ajax({
+                  url: restAPIURL,
+                  type: 'POST',
+                  headers: {
+                    "Authorization": "Bearer " + access_token,
+                    "Content-Type": "application/x-www-form-urlencoded"
+                  },
+                  data: $.param({
+                    "partnernumber": partnernumber
+                  }),
+                  async: true,
+                  timeout: 0,
+                  contentType: 'application/x-www-form-urlencoded',
+                  success: function (data) {
+                    this_.runNext();
+                    console.log(data);
+                    _score = data;
 
-          return Controller.extend("ifm.hack.initial", {
+                    that._firePropertiesChanged();
+                    this.settings = {};
+                    this.settings.score = "";
 
-            onInit: function (oEvent) {
+                    that.dispatchEvent(new CustomEvent("onStart", {
+                      detail: {
+                        settings: this.settings
+                      }
+                    }));
 
-            },
+                  },
+                  error: function (e) {
+                    this_.runNext();
+                    console.log("error: " + e);
+                    console.log(e);
+                  }
+                });
 
-            onButtonPress: function (oEvent) {
-              var this_ = this;
-
-              var partnernumber = oView.byId("input").getValue();
-              console.log(partnernumber);
-              this_.wasteTime();
-
-              var CLIENT_ID_str = 'REPLACE_WITH_CLIENT_ID';
-              var CLIENT_SECRET_str = 'REPLACE_WITH_CLIENT_SECRET';
-
-              $.ajax({
-                type: 'POST',
-                url: "https://REPLACE_WITH_TOKEN_URL/uaa-security/oauth/token",
-                contentType: 'application/x-www-form-urlencoded; charset=utf-8',
-                crossDomain: true,
-                cache: true,
-                dataType: 'json',
-                data: {
-                  client_id: CLIENT_ID_str,
-                  client_secret: CLIENT_SECRET_str,
-                  grant_type: 'client_credentials',
-                },
-
-                success: function (data) {
-                  console.log(data);
-
-                  var access_token = data.access_token;
-
-                  $.ajax({
-                    url: restAPIURL,
-                    type: 'POST',
-                    headers: {
-                      "Authorization": "Bearer " + access_token,
-                      "Content-Type": "application/x-www-form-urlencoded"
-                    },
-                    data: $.param({
-                      "partnernumber": partnernumber
-                    }),
-                    async: true,
-                    timeout: 0,
-                    contentType: 'application/x-www-form-urlencoded',
-                    success: function (data) {
-                      this_.runNext();
-                      console.log(data);
-                      _score = data;
-
-                      that._firePropertiesChanged();
-                      this.settings = {};
-                      this.settings.score = "";
-
-                      that.dispatchEvent(new CustomEvent("onStart", {
-                        detail: {
-                          settings: this.settings
-                        }
-                      }));
-
-                    },
-                    error: function (e) {
-                      this_.runNext();
-                      console.log("error: " + e);
-                      console.log(e);
-                    }
-                  });
-
-                },
-                error: function (e) {
-                  this_.runNext();
-                  console.log(e.responseText);
-                }
-              });
-            }
-
-          });
+              },
+              error: function (e) {
+                this_.runNext();
+                console.log(e.responseText);
+              }
+            });
+          }
 
         });
 
-      //### THE APP: place the XMLView somewhere into DOM ###
-      var oView = new sap.ui.core.mvc.XMLView({
-        viewContent: jQuery(_shadowRoot.getElementById("oView")).html(),
       });
-      oView.placeAt(content);
 
-    }
+    //### THE APP: place the XMLView somewhere into DOM ###
+    var oView = new sap.ui.core.mvc.XMLView({
+      viewContent: jQuery(_shadowRoot.getElementById("oView")).html(),
+    });
+    oView.placeAt(content);
+
   }
-  customElements.define("com-ifm-datasphere", IFMDataSphere);
-
-})();
+}
