42977d633d539eb37d42cb5c8973849897b3fd5b
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-06-29 22:03:53 +0200

    u

--- src/components/DataSphere.js
+++ src/components/DataSphere.js
@@ -207,7 +207,7 @@ export default class IFMDataSphere extends HTMLElement {
       }
     });
 
-    xhr.open("GET", this._export_settings.restapiurl + "/sap/bc/ina/service/v2/GetServerInfo"); //https://dwc-infomotion.eu10.hcs.cloud.sap/sap/bc/ina/service/v2/GetServerInfo"
+    xhr.open("GET", this._export_settings.restapiurl + "sap/bc/ina/service/v2/GetServerInfo");
 
     //adding request headers
     xhr.setRequestHeader("x-csrf-token", "Fetch");
